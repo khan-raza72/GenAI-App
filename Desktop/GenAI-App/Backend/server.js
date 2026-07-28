7e112bcc6ead05ba9b59006def33ff7e4d6baa02
Raza khan <mursalrazakhan786@gmail.com>
2026-07-28 11:18:16 +0530

    fix: updated cors and vercel config

--- Desktop/GenAI-App/Backend/server.js
+++ Desktop/GenAI-App/Backend/server.js
@@ -1,16 +1,22 @@
+require("dotenv").config(); // Load environment variables first
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
+import cors from "cors";
 
-require('dotenv').config(); // Load environment variables first
-const app = require('./src/app');
-const connectToDB = require('./src/config/database');
-
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://gen-ai-app-sigma.vercel.app"],
+    credentials: true,
+  }),
+);
 
 const PORT = process.env.PORT || 3000;
 
 // Pehle Local MongoDB start hoga, uske baad server port listen karega
 connectToDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`🚀 Custom Architecture Server is running on http://localhost:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(
+      `🚀 Custom Architecture Server is running on http://localhost:${PORT}`,
+    );
+  });
 });
-
--- Desktop/GenAI-App/Frontend/src/features/auth/services/auth.api.js
+++ Desktop/GenAI-App/Frontend/src/features/auth/services/auth.api.js
@@ -2,9 +2,10 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
-})
+  // VITE_API_BASE_URL check karega, agar na mile toh direct Render backend URL use karega
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://genai-app-3w8f.onrender.com/api",
+  withCredentials: true,
+});
 
 export async function register({ username, email, password }) {
 
